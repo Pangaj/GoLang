c590c30fd8d67f075d850b4b483a2c53bbbe46ca
Pangaj <pangaj555@gmail.com>
2018-01-08 01:39:04 +0530

    create a new file through GoLang

---
+++ Files/CreateNewFile.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+	"fmt"
+)
+
+/**
+ * Created by Pan on 1/8/2018.
+ */
+func main() {
+	//os.Create will create a new file
+	//If already exists, then truncates the file
+	file, err := os.Create("NewFile.txt")
+
+	if err != nil {
+		fmt.Println("Error in creating file")
+	}
+
+	file.Write([]byte("New File Created :)"))
+}
