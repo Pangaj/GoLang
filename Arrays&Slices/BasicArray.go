8b8e63b741a21bea5dca40f7586a6de63a2dd0ce
Pangaj <pangaj555@gmail.com>
2018-01-12 12:43:49 +0530

    basic array programming

---
+++ Arrays&Slices/BasicArray.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+/**
+ * Created by Pan on 1/11/2018.
+ */
+func main() {
+	//An array is a numbered sequence of elements of a single type with a fixed length
+	var arr [5]int
+
+	arr[0] = 1
+	arr[1] = 2
+	arr[3] = 4
+	arr[4] = 5
+
+	/*
+	 *	Or directly initialise as below
+	 * 	var arr = [5]int{1,2,3,4,5}
+	 *  arr := [5]int{1,2,3,4,5}
+	 */
+
+	//output : [1 2 0 4 5]
+	fmt.Println(arr)
+
+	//output : 5
+	fmt.Println(arr[4])
+}
